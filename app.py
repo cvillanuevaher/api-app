e67799818eb4f093e71864e52f7c7ddf92a6e3d3
cvillanuevaher <carlos.villanueva.hernandez@sqm.com>
2024-12-11 18:31:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def read_root():
 
 from datetime import datetime
 
-@app.get("/api/stock")
+@app.get("/api/stock2")
 def get_stock(
     fecha: str = Query(..., description="Fecha del movimiento (YYYY-MM-DD)"),
     codigos_centros: list[str] = Query(..., description="Lista de códigos de centros"),
