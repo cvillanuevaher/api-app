0b03aaa13d0cdc532f82b84ea584cce641c18726
cvillanuevaher <carlos.villanueva.hernandez@sqm.com>
2024-12-11 17:04:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ load_dotenv()
 
 app = FastAPI()
 
-# Configuración segura de Databricks desde variables de entorno
+# Cargar variables de entorno desde Azure o .env
 server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME")
 http_path = os.getenv("DATABRICKS_HTTP_PATH")
 access_token = os.getenv("DATABRICKS_ACCESS_TOKEN")
+
+# Validar que las variables no sean None
+if not server_hostname or not http_path or not access_token:
+    raise ValueError("Faltan variables de entorno: Verifica DATABRICKS_SERVER_HOSTNAME, HTTP_PATH y ACCESS_TOKEN")
 
 @app.get("/")
 def read_root():
